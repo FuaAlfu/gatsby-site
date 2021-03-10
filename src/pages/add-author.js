d448daa1799fbff7265937c06839f2cb18805a77
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-10 18:37:22 +0300

    add

--- src/pages/add-author.js
+++ src/pages/add-author.js
@@ -1,18 +1,32 @@
-import React, {useState} from 'react';
+import React, {useState,useContext} from 'react';
 import {Form, Input, Button} from './components/common';
+import {FirebaseContext} from '../components/Firebase'
 
 const AddAuthor =  () => {
+    const {firebase} = useContext(FirebaseContext);
     const [authorName,setAuthorName] = useState('');
+    const [success, setSuccess] = useState(false);
     function handleSubmit(){
         e.preventDefault();
-        
+        firebase.ccreateAuthor({
+            authorName
+        }).then(() =>{
+            setAuthorName('');
+            setSuccess(true);
+        })
     }
         return (
             <Form onSubmit={handleSubmit}>
                 <Input onChange={(e) =>{
                     e.persist();
+                    setSuccess(false);
                     setAuthorName(e.target.vale);
                 }} value={authorName} placeholder="author name"/>
+                {!!success && 
+                <span>
+                  Author created succesfully.
+                </span>
+                }
                 <Button type="submit" block>
                   Add new Author
                 </Button>
