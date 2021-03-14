b1537a93184b7af0e8d8b65dae102a93bfa3993c
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-14 17:43:47 +0300

    add

--- src/pages/add-note.js
+++ src/pages/add-note.js
@@ -1,5 +1,12 @@
 import React, {useContext,useState,useEffect} from 'react';
-import {FirebaseContext} from '../components/Firebase'
+import {FirebaseContext} from '../components/Firebase';
+import {Form, Input, Button} from '../components/common';
+import styled from 'styled-components';
+
+const FormField = styled.div`
+margin-bottom: 20px;
+
+`;
 
 const AddNote = () => {
     //destructor firebase
@@ -26,7 +33,36 @@ const AddNote = () => {
     },[firebase]);
 
     console.log(authors);
-    return (<div />) //empty div
+   // return (<div />) //empty div
+   return(
+
+    <Form>
+    <FormField>
+     <Input placeholder="note name"/>
+    </FormField>
+    {/*end of input*/}
+    <FormField>
+    <strong>Author</strong>
+    <div>
+     <select >
+       {authors.map(a => {
+          <option key={a.id} value={a.id}>
+             {a.name}
+          </option>
+       })}
+     </select>
+     </div>
+    </FormField>
+    {/*end of select*/}
+    <FormField>
+       <strong>Note Cover</strong>
+       <Input type="file"/>
+    </FormField>
+    <Button block type="submit">
+       Add new note
+    </Button>
+    </Form>
+   )
 }
 
 export default AddNote;
